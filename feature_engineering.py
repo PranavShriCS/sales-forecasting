fd5d53a0920fdf6b5eda39ead1820ec4fadf0c10
Pranav Shri GS <gspranavshri@gmail.com>
2026-09-05 21:47:39 +0530

    Complete feature engineering

--- feature_engineering.py
+++ feature_engineering.py
@@ -4,7 +4,13 @@ path = r"C:\Users\gopin\.cache\kagglehub\competitions\walmart-recruiting-store-s
 
 train = pd.read_csv(path + r"\train.csv")
 
+stores = pd.read_csv(path + r"\stores.csv")
+
+features = pd.read_csv(path + r"\features.csv")
+
 train["Date"] = pd.to_datetime(train["Date"])
+
+features["Date"] = pd.to_datetime(features["Date"])
 
 # Sort chronologically within each store and department
 train = train.sort_values(["Store", "Dept", "Date"])
@@ -20,4 +26,43 @@ train["Lag_1"] = (
          .shift(1)
 )
 
-print(train.head(15))
+train["Rolling_Mean_4"] = (
+    train.groupby(["Store", "Dept"])["Weekly_Sales"]
+         .transform(lambda x: x.shift(1).rolling(4).mean())
+)
+
+train["Rolling_Mean_8"] = (
+    train.groupby(["Store", "Dept"])["Weekly_Sales"]
+         .transform(lambda x: x.shift(1).rolling(8).mean())
+)
+
+train = train.merge(
+    stores,
+    on="Store",
+    how="left"
+)
+
+train = train.merge(
+    features,
+    on=["Store", "Date"],
+    how="left"
+)
+
+train = train.drop(columns=["IsHoliday_y"])
+train = train.rename(columns={"IsHoliday_x": "IsHoliday"})
+
+print(train.head())
+print(train.columns)
+
+markdown_cols = [
+    "MarkDown1",
+    "MarkDown2",
+    "MarkDown3",
+    "MarkDown4",
+    "MarkDown5"
+]
+
+train[markdown_cols] = train[markdown_cols].fillna(0)
+
+print(train.isnull().sum())
+
